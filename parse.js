b066912a07a8dbd548b301f00ee352a91ddd1b94
inishcheretova <devsilvanasha@gmail.com>
2018-07-15 03:18:30 +0300

    added parsing promise chain

--- parse.js
+++ parse.js
@@ -6,53 +6,15 @@ const countAds = process.argv[2] ? process.argv[2] : 50;
 const options = {
     follow_max: 5,
     follow_set_cookies: true,
-}
+};
 
 const countParam = countAds <= 20 ? '' : countAds < 60 ? "search/list=list/page_len40/" : "search/list=list/page_len60/";
 
 const url = 'http://irr.by/realestate/longtime/' + countParam;
 
+let adsPromise = getAdsByPageUrl(url, countAds);
 
-let promise = new Promise((resolve, reject) => {
-
-});
-
-let ads = getAdsByPageUrl(url);
-
-// console.log(ads);
-
-// while (ads.length < countAds) {
-//     const nextPageUrl = getNextPageUrl($);
-//     getAdsByPageUrl(hostName + nextPageUrl);
-// }
-
-// needle.get(url, options, (error, response) => {
-//     // if (error) throw err;
-//     // if (!error && response.statusCode == 200)
-//     //
-//     const $ = cheerio.load(response.body);
-//     const ads = $('.add_list');
-//
-//     console.log(ads);
-//
-//     const adsObjects = [];
-//
-//     while (ads.length < countAds) {
-//         const nextPageUrl = getNextPageUrl($);
-//         getAdsByPageUrl(hostName + nextPageUrl);
-//     }
-//
-//     ads.each((i, ad) => {
-//         const adWrapper = $(ad);
-//         const src = adWrapper.find('.add_title').attr('href');
-//
-//         let adObject = parseAd(src);
-//
-//         // adObject.date = adWrapper.find('.add_data').text();
-//         // console.log( adWrapper.find('.add_data').text());
-//     });
-//     // callback();
-// });
+adsPromise.then((ads) => console.log(ads));
 
 function parseAd(src) {
     return new Promise((resolve, reject) => {
@@ -70,39 +32,46 @@ function parseAd(src) {
     });
 }
 
-function getAdsByPageUrl(pageUrl) {
-    let ads = [];
+function getAdsByPageUrl(pageUrl, remaining) {
 
-     needle.get(pageUrl, options, (error, response) => {
-         const $ = cheerio.load(response.body);
-//     const ads = $('.add_list');
-
-        while (ads.length < countAds) {
-            const nextPageUrl = getNextPageUrl($);
-            new Promise((resolve, reject) => {
-                getAdsByPageUrl(hostName + nextPageUrl);
-            })
-        }
-        // console.log(ads);
+    return needle('get', pageUrl, options).then((response) => {
+        const $ = cheerio.load(response.body);
+        const ads = $('.add_list');
+        let adsSet = [];
 
         ads.each((i, ad) => {
-            const adWrapper = $(ad);
-            const src = adWrapper.find('.add_title').attr('href');
+            let adCheckPromise = new Promise((resolve, reject) => {
+                const adWrapper = $(ad);
+                const src = adWrapper.find('.add_title').attr('href');
 
-           // let adObject = {};
-           parseAd(src).then( adObjectResult => {
-               const adObject = adObjectResult;
-               console.log(adObject);
-           });
-
-
-
-            // adObject.date = adWrapper.find('.add_data').text();
-            // console.log( adWrapper.find('.add_data').text());
+                parseAd(src).then((adObject) => {
+                    adObject.date = adWrapper.find('.add_data').text();
+                    resolve(adObject);
+                });
+            });
+            adsSet.push(adCheckPromise);
         });
-    });
 
-    return ads;
+        remaining -= ads.length;
+
+        let adsPromise;
+
+        if (remaining > 0) {
+            const nextPageUrl = getNextPageUrl($);
+            adsPromise = getAdsByPageUrl(hostName + nextPageUrl, remaining);
+        }
+
+        return new Promise((resolve, reject) => {
+            if (adsPromise) {
+                adsPromise.then((prevAds) => {
+                    adsSet = adsSet.concat(prevAds);
+                    resolve(adsSet);
+                })
+            } else {
+                Promise.all(adsSet).then((parsedAds) => resolve(parsedAds));
+            }
+        }).then((adsl) => adsl);
+    });
 }
 
 function getNextPageUrl($) {
